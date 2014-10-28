5c530e201f9f6a158e72b39f5138ffe8f225e5d5
Miklos Espak <m.espak@ucl.ac.uk>
2014-10-28 14:06:34 +0000

    Check fixed about active milestones

--- migrate.py
+++ migrate.py
@@ -109,7 +109,7 @@ def convert_issues(source, dest, dest_project_id):
         new_milestone = Milestones(
             description = milestone['description'],
             title = milestone['name'],
-            state = 'active' if not 'completed' in milestone.keys()  else 'completed'
+            state = 'active' if str(milestone['completed']) == '0'  else 'completed'
         )
         if method == 'direct':
             new_milestone.project = dest_project_id
